2d3d0d70f49dce5c6b006463e3ae2458b251fa1d
ch1ckenD4nce <hththam22@gmail.com>
2025-05-28 06:51:51 +0700

    config ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -59,6 +59,9 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DB,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: [
         User,
         EmailTemplate,
